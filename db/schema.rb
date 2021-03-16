6f124c5b9da63e3fc380eb0903be9067fd317942
Guillaume Lafond <g.lafond@gmail.com>
2021-03-16 15:41:33 +0100

    migration adding columns to Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_114633) do
+ActiveRecord::Schema.define(version: 2021_03_16_143741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2021_03_16_114633) do
     t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.string "author"
+    t.string "pageCount"
+    t.string "thumbnailUrl"
   end
 
   create_table "libraries", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+# Creates 3 users
+  # manually entered data :
+    # "first_name" "last_name" "username" "email" "bio"
+    # "geolocation" (V1 = city, V2 = address)
+  # photo ?
+
+# Create 1 Library for each User : "user_id"
+
+# Creates 30 books
+  # Find 30 ISBN exact code
+  # Find the book with Google Api, Get the JSON file
+  # Parse JSON and create the Instance
+    # "ISBN" "title" "year" ("publishedDate") "genre" "language"
+    # needed : description, author, page_count, thumbnail_url, google_id
+
+
+
+# Creates 10 x 3 library_books
+  # Associate the Instance of the Book to A random User
+  # Add a "book_condition" for each library_book
+
+
