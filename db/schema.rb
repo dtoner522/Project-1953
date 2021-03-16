a37cf8d810a14772d52bafd172a9e8a5ca55cf51
Guillaume Lafond <g.lafond@gmail.com>
2021-03-16 17:14:41 +0100

    added condition to library + first version of seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_143741) do
+ActiveRecord::Schema.define(version: 2021_03_16_155636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_143741) do
     t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "condition"
     t.index ["book_id"], name: "index_library_books_on_book_id"
     t.index ["library_id"], name: "index_library_books_on_library_id"
   end
@@ -67,7 +68,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_143741) do
     t.string "first_name"
     t.string "last_name"
     t.text "bio"
-    t.string "geolocation"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
