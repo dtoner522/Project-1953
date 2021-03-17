7cf5b0e762213df380cf3aa318b6ad7888e94444
daire toner <dtoner522@gmail.com>
2021-03-17 17:14:12 +0100

    library books index working and displaying titles

--- app/controllers/library_books_controller.rb
+++ app/controllers/library_books_controller.rb
@@ -1,2 +1,11 @@
 class LibraryBooksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @library_books = LibraryBook.all
+  end
+
+  def show
+  end
+
 end
