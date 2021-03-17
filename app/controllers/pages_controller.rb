84edbae277964f1f4d6c23975e0a0418fa83c6b6
Eula Gonzales <eulabgm@gmail.com>
2021-03-17 18:24:00 +0100

    Pages frontend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :uikit ]
+  skip_before_action :authenticate_user!, only: [ :home, :uikit, :results ]
 
   def home
   end
 
   def uikit
+  end
+
+  def results
   end
 end
