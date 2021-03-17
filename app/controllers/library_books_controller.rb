98d221faaef8104e7de1e822281393dd74301c32
Guillaume Lafond <g.lafond@gmail.com>
2021-03-17 18:10:15 +0100

    adding search autocomplete + click listener

--- app/controllers/library_books_controller.rb
+++ app/controllers/library_books_controller.rb
@@ -1,2 +1,12 @@
 class LibraryBooksController < ApplicationController
+
+
+  def create
+    @book = Book.new()
+    @library_book = LibraryBook.new(book: @book, library_id: current_user.library)
+
+  end
+
+
+
 end
