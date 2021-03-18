3941aae5bfeb5e60d38b06eae642b8061c634f6a
agustina ohan <agustina.oh@gmail.com>
2021-03-18 17:50:40 +0100

    links into chatrooms

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -9,13 +9,6 @@ class Chatroom < ApplicationRecord
     sender == current_user ? recipient : sender
   end
 
-  # def last_message_time(chatroom)
-  #   sent = chatroom.messages.last.created_at
-  #   now = DateTime.new
-  #   now - sent
-  # end
-
-
   def self.existing(recipient_user, current_user, book)
     where(recipient: recipient_user, sender: current_user, book: book)
   end
