b916f1feb1457a0afc5cbaa02177c59f9a824abd
Guillaume Lafond <g.lafond@gmail.com>
2021-03-22 18:36:25 +0100

    wishlist v2 : adding from show page, delete from profile

--- app/controllers/library_books_controller.rb
+++ app/controllers/library_books_controller.rb
@@ -3,13 +3,12 @@ class LibraryBooksController < ApplicationController
 
   def index
     @library_books = LibraryBook.all
-    @library_books = LibraryBook.select {|lb| lb.library.user.location.downcase == params[:query].downcase} if params[:query].present?
+    @library_books = LibraryBook.select { |lb| lb.library.user.location.downcase == params[:query].downcase } if params[:query].present?
 
     @library_books = @library_books.joins(:book).where("books.title ILIKE ? ", "%#{params[:title]}%") if params[:title].present?
     @library_books = @library_books.joins(:book).where("books.language ILIKE ? ", params[:language]) if params[:language].present?
     @library_books = @library_books.joins(:book).where("books.genre ILIKE ? ", params[:genre]) if params[:genre].present?
   end
-
 
   def destroy
     @library_book = LibraryBook.find(params[:id])
@@ -18,10 +17,11 @@ class LibraryBooksController < ApplicationController
     redirect_to user_path(current_user)
   end
 
-
   def show
     @lib_book = LibraryBook.find(params[:id])
     @chatroom = Chatroom.new
+    @wishlist_book = WishlistBook.new
+    @book = @lib_book.book
   end
 
 end
