02c181c4af12696cb581a86c31f557b2501874da
Guillaume Lafond <g.lafond@gmail.com>
2021-03-23 17:22:02 +0100

    wishlist add from index / missing heart color change

--- app/controllers/wishlist_books_controller.rb
+++ app/controllers/wishlist_books_controller.rb
@@ -3,7 +3,7 @@ class WishlistBooksController < ApplicationController
   def destroy
     @wishlist_book = WishlistBook.find(params[:id])
     @wishlist_book.destroy
-    flash[:alert] = "Added to your wishlist"
+    flash[:alert] = "Removed from your wishlist"
     redirect_back(fallback_location: root_path)
   end
 
@@ -12,7 +12,7 @@ class WishlistBooksController < ApplicationController
     @wishlist_book.book_id = params[:book_id]
     @wishlist_book.wishlist_id = params[:wishlist_id]
     if @wishlist_book.save
-      flash.alert = "Added"
+      flash.alert = "Added to your wishlist !"
       redirect_back(fallback_location: root_path)
     end
   end
--- app/models/library_book.rb
+++ app/models/library_book.rb
@@ -8,4 +8,9 @@ class LibraryBook < ApplicationRecord
   def is_favoritable_to(user)
     (self.library.user != user) || !(user.libraries.first.library_books.any? {|lb| lb.book.title == self.book.title})
   end
+
+  def is_in_wishlist(lib_book, user)
+    user.wishlists.first.wishlist_books.find { |wbbook| wbbook.book.id == lib_book.book.id }
+    # we have an instance of a book -> find the first wishlist_book from that user with that Book
+  end
 end
