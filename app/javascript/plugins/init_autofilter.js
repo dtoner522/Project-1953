dc7bbbc50d546b087e0a51a4a47c3c944b566d4e
daire toner <dtoner522@gmail.com>
2021-03-25 10:09:59 +0100

    console log removed

--- app/javascript/plugins/init_autofilter.js
+++ app/javascript/plugins/init_autofilter.js
@@ -1,23 +1,21 @@
 const initAutoFilter = () => {
-    const form = document.querySelector("form")
+  const form = document.querySelector("form")
     if (form) {
-        const languageSelect = document.querySelector("#language-select");
-        const genreSelect = document.querySelector("#genre-select");
+      const languageSelect = document.querySelector("#language-select");
+      const genreSelect = document.querySelector("#genre-select");
 
-        console.log(languageSelect)
+      $(languageSelect).on('select2:close', (event) => {
+          console.log(event.params.data)
+        event.preventDefault();
+        document.querySelector("form").submit()
+      });
 
-        $(languageSelect).on('select2:close', (event) => {
-            console.log(event.params.data)
-          event.preventDefault();
-          document.querySelector("form").submit()
-        });
-
-        $(genreSelect).on('select2:close', (event) => {
-            console.log(event.params.data)
-          event.preventDefault();
-          document.querySelector("form").submit()
-        });
-    }
+      $(genreSelect).on('select2:close', (event) => {
+          console.log(event.params.data)
+        event.preventDefault();
+        document.querySelector("form").submit()
+      });
+  }
 }
 
 export { initAutoFilter };
